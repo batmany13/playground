2d0114c03813aecc20bdc4320583f80bc77e2448
Bruce Wang <byyw13@gmail.com>
2017-07-31 18:43:53 -0700

    update to include vendors as well

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
 	"strings"
 	"time"
 
@@ -10,7 +11,8 @@ import (
 )
 
 const (
-	URL          = "https://gist.githubusercontent.com/huned/1d1c076eac47b34a5e2b0b0aab4aea65/raw/dc187a07d2899bb06d71c06c2008f7aa7406d6d1/meals.json.txt"
+	MEAL_URL     = "https://gist.githubusercontent.com/huned/1d1c076eac47b34a5e2b0b0aab4aea65/raw/dc187a07d2899bb06d71c06c2008f7aa7406d6d1/meals.json.txt"
+	VENDOR_URL   = "https://gist.githubusercontent.com/huned/67b0fb14c4190c76abf3796d28f6811b/raw/9f4d846bca8b3ff8c3f2dd02f8d719ceaefdbd93/vendors.json"
 	RESULT_FILE  = "results.json"
 	RESULT_FILE2 = "results2.json"
 	TIME_LAYOUT  = `2006-01-02 15:04`
@@ -20,8 +22,17 @@ type MealTime struct {
 	time.Time
 }
 
+type VendorResult struct {
+	Vendors []Vendor `json:"results"`
+}
+
 type Meal struct {
 	Meals []Result `json:"results"`
+}
+
+type Vendor struct {
+	VendorId int `json:"vendor_id"`
+	Drivers  int `json:"drivers"`
 }
 
 type Result struct {
@@ -41,14 +52,28 @@ func (m *MealTime) UnmarshalJSON(b []byte) (err error) {
 	return
 }
 
+func DriverAvail() (map[int]int, error) {
+	var vendor VendorResult
+	vendorAvail := make(map[int]int)
+	resp, err := http.Get(VENDOR_URL)
+	if err != nil {
+		return vendorAvail, err
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return vendorAvail, err
+	}
+	json.Unmarshal(data, &vendor)
+	for _, ven := range vendor.Vendors {
+		vendorAvail[ven.VendorId] = ven.Drivers - 1
+	}
+	return vendorAvail, nil
+}
+
 func (v *VendorReq) Available() (bool, error) {
 	var meal Meal
-	// resp, err := http.Get(URL)
-	// if err != nil {
-	// 	return false, err
-	// }
+	vendorAvail, _ := DriverAvail()
 	data, err := ioutil.ReadFile(RESULT_FILE)
-	// data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return false, err
 	}
@@ -60,17 +85,36 @@ func (v *VendorReq) Available() (bool, error) {
 		start := res.Datetime.Add(-20 * time.Minute)
 		end := res.Datetime.Add(10 * time.Minute)
 		if v.Timestamp.Equal(start) || v.Timestamp.Equal(end) {
-			return false, nil
+			if vendorAvail[v.VendorId] == 0 {
+				return false, nil
+			}
+			vendorAvail[v.VendorId] = vendorAvail[v.VendorId] - 1
+			continue
 		}
 		if v.Timestamp.After(start) && v.Timestamp.Before(end) {
-			return false, nil
+			if vendorAvail[v.VendorId] == 0 {
+				return false, nil
+			}
+			vendorAvail[v.VendorId] = vendorAvail[v.VendorId] - 1
+			continue
 		}
 	}
 	return true, nil
 }
 
 func vendorAvail(c *gin.Context) {
-	// ret, err := vendor.Availabe()
+	var v VendorReq
+	c.BindJSON(&v)
+	ret, err := v.Available()
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": err.Error,
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"result": ret,
+	})
 }
 
 func setupRouter() {
--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 var FAIL_TIME = []string{"2017-01-01 11:10", "2017-01-01 11:40", "2017-01-01 12:10", "2017-01-01 12:40"}
+var FAIL_TIME2 = []string{"2017-01-01 11:40", "2017-01-01 12:10"}
 var SUCCESS_TIME = []string{"2017-01-01 10:30"}
 
 func TestAvailable(t *testing.T) {
@@ -19,6 +20,25 @@ func TestAvailable(t *testing.T) {
 		req := VendorReq{VendorId: 1, Timestamp: mt}
 		ret, err := req.Available()
 		assert.Nil(err)
+		assert.True(ret)
+	}
+	for _, fail := range FAIL_TIME2 {
+		mt := MealTime{}
+		check, _ := time.Parse(TIME_LAYOUT, fail)
+		mt.Time = check
+		req := VendorReq{VendorId: 3, Timestamp: mt}
+		ret, err := req.Available()
+		assert.Nil(err)
 		assert.False(ret)
+	}
+
+	for _, fail := range SUCCESS_TIME {
+		mt := MealTime{}
+		check, _ := time.Parse(TIME_LAYOUT, fail)
+		mt.Time = check
+		req := VendorReq{VendorId: 1, Timestamp: mt}
+		ret, err := req.Available()
+		assert.Nil(err)
+		assert.True(ret)
 	}
 }
